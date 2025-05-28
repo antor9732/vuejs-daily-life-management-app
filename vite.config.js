ac7f1fa19b198de978c3342b498588f55adc0823
Md. Mahmudul Hasan <antormahmud789@gmail.com>
2025-05-28 17:08:32 +0600

    .github/workflows/deploy.yml file added

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { fileURLToPath, URL } from 'node:url'
+import { createRouter, createWebHistory } from 'vue-router'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -16,4 +17,11 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+})
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    // your routes...
+  ]
 })
