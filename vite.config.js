8073ce76a2d1d31457e747c867ade9f847f35d3a
Md. Mahmudul Hasan <antormahmud789@gmail.com>
2025-05-28 17:32:08 +0600

    deploy error solve

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,17 @@
 import { fileURLToPath, URL } from 'node:url'
-import { createRouter, createWebHistory } from 'vue-router'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
   ],
-  base : '/vuejs-daily-life-management-app/',
+  base : '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-})
-
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    // your routes...
-  ]
 })
