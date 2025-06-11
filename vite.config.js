107c4473107a09652d0c3562b81c2c4438eb8936
Md. Mahmudul Hasan <antormahmud789@gmail.com>
2025-06-11 18:07:05 +0600

    Update build for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/',
+  base: '/vuejs-daily-life-management-app/', // Changed this line
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
